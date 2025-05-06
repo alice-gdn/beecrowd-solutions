a591aaf95cda833ae94b6769d028022d192bc481
Alice Galdino <alicegaldino741@gmail.com>
2025-05-06 11:35:44 -0300

    Update Ex1019.java

--- iniciante_java/Ex1019.java
+++ iniciante_java/Ex1019.java
@@ -1,10 +1,25 @@
 package exercicios_beecrowd;
 
+import java.util.Scanner;
+
 public class Ex1019 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner sc = new Scanner (System.in);
+		
+		 int duracaoSeg = sc.nextInt();
+		 
+		 
+		 System.out.print(duracaoSeg / 3600 + " : ");
+		 duracaoSeg = duracaoSeg % 3600;
+		 
+		 System.out.print(duracaoSeg / 60 + " : ");
+		 duracaoSeg = duracaoSeg % 60;
+		 
+		 System.out.println(duracaoSeg);
+		 
+		 sc.close();
 	}
 
 }
